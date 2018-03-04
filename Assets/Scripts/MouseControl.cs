6bae74f724f8f2fc37f6d45f97be5df736c06bb7
Ellie Richards <eleanor4.richards@live.uwe.ac.uk>
2018-03-04 18:27:41 +0000

    Point plotting

--- Assets/Scripts/MouseControl.cs
+++ Assets/Scripts/MouseControl.cs
@@ -10,6 +10,7 @@ public class MouseControl : MonoBehaviour
     private bool editMode = false;
     private GameObject textBox;
     private Text positionText;
+    public LayerMask acceptMask;
 
     // Use this for initialization
     private void Start()
@@ -17,7 +18,7 @@ public class MouseControl : MonoBehaviour
         cam = GameObject.Find("Main Camera").GetComponent<Camera>();
         pointPlotter = GetComponent<PointPlotter>();
         textBox = GameObject.Find("CursorPos");
-        positionText = textBox.GetComponent<Text>();
+        positionText = textBox.GetComponentInChildren<Text>();
     }
 
     // Update is called once per frame
@@ -27,7 +28,7 @@ public class MouseControl : MonoBehaviour
 
         Ray ray = cam.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.farClipPlane));
 
-        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity, 1 << LayerMask.NameToLayer("Graph"));
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity, acceptMask);
         Debug.DrawRay(origin, ray.direction, Color.red);
         // RaycastHit hit;
 
@@ -36,11 +37,23 @@ public class MouseControl : MonoBehaviour
             transform.localPosition = hit.point;
             textBox.transform.position = new Vector2(hit.point.x, hit.point.y + 0.3f);
             positionText.text = pointPlotter.ClickRecieved(hit.point).ToString();
-            if (Input.GetKey(KeyCode.Mouse0))
+            if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                pointPlotter.ClickRecieved(hit.point);
+                //pointPlotter.ClickRecieved(hit.point);
+                pointPlotter.PlacePoint(hit.point);
+            }
+
+            if (hit.transform.CompareTag("point"))
+            {
+                Debug.Log("removing d");
+                if (Input.GetKeyDown(KeyCode.Mouse1))
+                {
+                    Debug.Log("removing point...");
+                    pointPlotter.RemovePoint(hit.transform.gameObject);
+                }
             }
         }
+        //RaycastHit2D pointHit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity, acceptMask);
     }
 
     public void EditModeSwitch()
--- Assets/Scripts/PointPlotter.cs
+++ Assets/Scripts/PointPlotter.cs
@@ -11,10 +11,17 @@ public class PointPlotter : MonoBehaviour
     private Vector2 scaleValue;
     private Vector2 graphSpacePos;
 
+    private GameObject graphPoint;
+
+    private List<GameObject> points = new List<GameObject>();
+    private LineRenderer lineRend;
+
     // Use this for initialization
     private void Start()
     {
+        lineRend = GameObject.Find("GraphLine").GetComponent<LineRenderer>();
         graphHolder = GameObject.Find("GraphCanvas").GetComponent<AxisGen>();
+        graphPoint = Resources.Load("GraphPoint") as GameObject;
     }
 
     // Update is called once per frame
@@ -36,5 +43,47 @@ public class PointPlotter : MonoBehaviour
         graphSpacePos = new Vector2(_hitPoint.x / scaleValue.x, _hitPoint.y / scaleValue.y);
 
         return graphSpacePos;
+    }
+
+    public void PlacePoint(Vector2 worldPos)
+    {
+        GameObject curPoint = Instantiate(graphPoint, worldPos, Quaternion.identity);
+        curPoint.transform.SetParent(graphHolder.transform);
+        points.Add(curPoint);
+        LinkPoints();
+    }
+
+    public void RemovePoint(GameObject selected)
+    {
+        for (int i = 0; i < points.Count; i++)
+        {
+            if (points[i])
+            {
+                if (points[i] == selected)
+                {
+                    points.Remove(points[i]);
+                    Destroy(selected);
+                    // points.Sort();
+                }
+            }
+        }
+        LinkPoints();
+    }
+
+    private void LinkPoints()
+    {
+        int pointNum = points.Count;
+        if (pointNum > 1)
+        {
+            lineRend.positionCount = pointNum;
+            for (int i = 0; i < pointNum; i++)
+            {
+                lineRend.SetPosition(i, points[i].transform.position);
+            }
+            //Debug.DrawLine(points[pointNum].transform.position, points[pointNum - 1].transform.position, Color.green, 10.0f);
+        }
+        else
+        {
+        }
     }
 }
