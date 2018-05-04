b2022a1078f51c168b8c95e279cb92794dd364e4
Ellie Richards <eleanor4.richards@live.uwe.ac.uk>
2018-05-04 15:46:04 +0100

    Tab Input fields added

---
+++ Assets/Scripts/VitalTabManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VitalTabManager : MonoBehaviour
+{
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void PopulateVitalsList()
+    {
+        //pulse
+        //respiatory rate
+        //body temperature
+        //blood pressure
+        //blood sugar
+    }
+}
